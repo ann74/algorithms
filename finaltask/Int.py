0826eac595bfa9c05bcbe007d8aa6e98dae6ae57
Anna Lazarenko <oleglazarenko@yandex.ru>
2022-10-28 18:52:05 +0700

    Some improvements in Int

--- finaltask/Int.py
+++ finaltask/Int.py
@@ -2,15 +2,21 @@ class Int(int):
     """
     Класс дополняет встроенный тип int возможностью складывать int со строкой
     """
+    numbers = {'нуль': 0, 'один': 1, 'два': 2, 'три': 3, 'четыре': 4, 'пять': 5}
+
     def __add__(self, other):
         if isinstance(other, str):
-            numbers = {'нуль': 0, 'один': 1, 'два': 2, 'три': 3, 'четыре': 4, 'пять': 5}
             if other.isdigit():
-                return self.__int__() + int(other)
-            other = other.lower()
-            if other in numbers:
-                return self.__int__() + numbers[other]
-            raise TypeError('справа от знака "+" непонятный текст. Если что, я понимаю текстом цифры с 0 по 5.')
+                other = int(other)
+            else:
+                if other.lower() in self.numbers:
+                    other = self.numbers[other.lower()]
+                else:
+                    try:
+                        other = float(other)
+                        return self.__int__() + other
+                    except:
+                        raise TypeError('справа от знака "+" непонятный текст. Если что, я понимаю текстом цифры с 0 по 5.')
         return super().__add__(other)
 
 
@@ -18,6 +24,7 @@ if __name__ == '__main__':
     x = Int(5)
     print(x + 10)  # 15
     print(x + '5')  # 10
+    print(x + '12.5')  # 17.5
     print(x + 'один')  # 6
     print(x + 'пять')  # 10
     # print(x + 'шесть')  # TypeError: справа от знака "+" непонятный текст. Если что, я понимаю текстом цифры с 1 по 5.
